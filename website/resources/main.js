da7ebf2005e9e79c60516be1e43710ea1f0c262e
Aryan <aryanradadiya18@gmail.com>
2023-02-20 12:40:17 +0530

    update

--- website/resources/main.js
+++ website/resources/main.js
@@ -1,3 +1,16 @@
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+
+
 /*Back to top button*/
 const colorMax = 192;
 // gets the breakpoint at which the scroll-to-top button should appear
