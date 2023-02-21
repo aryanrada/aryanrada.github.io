b71383078e1dc9b1931ba8302c05ab9196bcaca5
Aryan <aryanradadiya18@gmail.com>
2023-02-21 10:58:22 +0530

    main

--- website/resources/main.js
+++ website/resources/main.js
@@ -81,3 +81,25 @@ function SVS_B(eAmt, where) {
     window.scrollBy(0, eAmt);
   }    
 }
+
+//Alert message in submit button
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('Thanks for your time and efforts, I will get back to you as soon as possible.', 'success')
+  })
+}
