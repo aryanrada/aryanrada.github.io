423bd2d1ab67b79efc3a737f89126e71689eb706
Aryan <aryanradadiya18@gmail.com>
2023-04-10 09:49:24 +0530

    data visualization page

--- resources/main.js
+++ resources/main.js
@@ -1,7 +1,5 @@
-// When the user scrolls down 20px from the top of the document, slide down the navbar
-// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+//Navigation Bar
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 0) {
     document.getElementById("navbar").style.top = "0";
